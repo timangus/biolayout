ed1650614729400ff92d15428e2f157dbc3abf68
Tim Angus <tim@ngus.net>
2012-12-07 12:28:53 +0000

    More generalised and comprehensive Java report

--- src/main/java/org/BioLayoutExpress3D/CoreUI/Dialogs/LayoutJavaPlatformCapsDialog.java
+++ src/main/java/org/BioLayoutExpress3D/CoreUI/Dialogs/LayoutJavaPlatformCapsDialog.java
@@ -3,6 +3,7 @@ package org.BioLayoutExpress3D.CoreUI.Dialogs;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 import static org.BioLayoutExpress3D.Environment.GlobalEnvironment.*;
 
 /**
@@ -14,12 +15,12 @@ import static org.BioLayoutExpress3D.Environment.GlobalEnvironment.*;
 */
 
 public final class LayoutJavaPlatformCapsDialog extends JDialog implements ActionListener
-{ 
-    /** 
+{
+    /**
     *  Serial version UID variable for the LayoutJavaPlatformCapsDialog class.
-    */        
+    */
     public static final long serialVersionUID = 111222333444555696L;
-    
+
     private AbstractAction javaPlatformCapsAction = null;
     private JButton okButton = null;
 
@@ -28,7 +29,7 @@ public final class LayoutJavaPlatformCapsDialog extends JDialog implements Actio
         super(jframe, "Java Platform Capabilities", true);
 
         initActions();
-        initComponents();        
+        initComponents();
     }
 
     private void initActions()
@@ -47,35 +48,26 @@ public final class LayoutJavaPlatformCapsDialog extends JDialog implements Actio
                 setVisible(true);
             }
         };
-    }    
-    
+    }
+
     private void initComponents()
-    {        
+    {
         JPanel topPanel = new JPanel(true);
-        topPanel.setLayout( new BorderLayout() );        
+        topPanel.setLayout( new BorderLayout() );
 
-        JTextArea textArea1 = new JTextArea();
-        textArea1.setFont( this.getFont() );
-        setJavaPlatformCaps(textArea1);
-        textArea1.setEditable(false);
-        textArea1.setCaretPosition(0); // so as to have the vertical scrollbar resetted to position 0
-        JScrollPane scrollPane1 = new JScrollPane(textArea1);
-
-        JTextArea textArea2 = new JTextArea();
-        textArea2.setFont( this.getFont() );
-        setDetailedJavaPlatformCaps(textArea2);
-        textArea2.setEditable(false);
-        textArea2.setCaretPosition(0); // so as to have the vertical scrollbar resetted to position 0
-        JScrollPane scrollPane2 = new JScrollPane(textArea2);
+        JTextArea textArea = new JTextArea();
+        textArea.setFont( Font.decode("Monospaced") );
+        setJavaPlatformCaps(textArea);
+        textArea.setEditable(false);
+        textArea.setCaretPosition(0); // so as to have the vertical scrollbar reset to position 0
+        JScrollPane scrollPane = new JScrollPane(textArea);
 
         okButton = new JButton("OK");
         okButton.addActionListener(this);
         okButton.setToolTipText("OK");
 
-        scrollPane1.setBorder( BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Java Platform Capabilities") );
-        topPanel.add(scrollPane1, BorderLayout.NORTH);
-        scrollPane2.setBorder( BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Detailed Java Platform Capabilities") );
-        topPanel.add(scrollPane2, BorderLayout.CENTER);
+        scrollPane.setBorder( BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Java Platform Capabilities") );
+        topPanel.add(scrollPane, BorderLayout.CENTER);
         topPanel.add(okButton, BorderLayout.SOUTH);
 
         this.getContentPane().add(topPanel);
@@ -86,73 +78,69 @@ public final class LayoutJavaPlatformCapsDialog extends JDialog implements Actio
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
+    private void appendPropertiesToTextArea(Properties properties, JTextArea textArea)
+    {
+        Set<String> keySet = properties.stringPropertyNames();
+        ArrayList keyList = new ArrayList(keySet);
+        Collections.sort(keyList);
+        int longestKey = 0;
+
+        for (Object keyObject : keyList)
+        {
+            String key = (String)keyObject;
+            if (key.length() > longestKey)
+                longestKey = key.length();
+        }
+
+        String pathSeparator = System.getProperty("path.separator");
+
+        for (Object keyObject : keyList)
+        {
+            String key = (String)keyObject;
+            int keyLength = key.length();
+            int padding = 1 + longestKey - keyLength;
+
+            String value = properties.getProperty(key);
+            value = value.replace("\n", "\\n");
+            value = value.replace("\r", "\\r");
+            value = value.replace("\t", "\\t");
+
+            textArea.append(key + ":");
+            for( int i = 0; i < padding; i++)
+                textArea.append(" ");
+
+            if (key.matches(".*(path|dirs)$"))
+            {
+                // Property is probably a list of paths, so split it up
+                String[] paths = value.split(pathSeparator);
+                textArea.append(paths[0] + "\n");
+
+                padding = 2 + longestKey;
+                for( int i = 1; i < paths.length; i++)
+                {
+                    for( int j = 0; j < padding; j++)
+                        textArea.append(" ");
+
+                    textArea.append(paths[i] + "\n");
+                }
+            }
+            else
+                textArea.append(value + "\n");
+        }
+    }
+
     private void setJavaPlatformCaps(JTextArea textArea)
     {
         int ONE_MB = 1 << 20; // (1024 * 1024);
 
-        textArea.append("\nOS Name:\t\t" + System.getProperty("os.name") + "\n");
-        textArea.append("OS Version:\t\t" + System.getProperty("os.version") + "\n");
-        textArea.append("OS Architecture:\t" + ( (IS_WIN) ? "\t" :"" ) + System.getProperty("os.arch") + "\n");
-        textArea.append("JVM bitness:\t\t" + (IS_64BIT ? "64 bit" : "32 bit") + "\n");
-        textArea.append("\n");
-        textArea.append("Java Runtime Name:\t" + System.getProperty("java.runtime.name") + "\n");
-        textArea.append("Java Runtime Version:\t" + System.getProperty("java.runtime.version") + "\n");
-        textArea.append("Java Version:\t\t" + System.getProperty("java.version") + "\n");
-        textArea.append("JVM Version:\t\t" + System.getProperty("java.vm.version") + "\n");        
-        textArea.append("JVM Name:\t\t" + System.getProperty("java.vm.name") + "\n");
-        textArea.append("JVM Vendor:\t\t" + System.getProperty("java.vm.vendor") + "\n");        
-        textArea.append("Available processor cores:\t" + RUNTIME.availableProcessors() + "\n");
-        textArea.append("Max JVM memory usage:\t" + (RUNTIME.maxMemory() / ONE_MB) + " MB\n");
-        textArea.append("Total JVM memory usage:\t" + (RUNTIME.totalMemory() / ONE_MB) + " MB\n");
-    }
+        Properties properties = new Properties();
+        properties.put("Detected JVM bitness", (IS_64BIT ? "64 bit" : "32 bit"));
+        properties.put("Number or cores", RUNTIME.availableProcessors());
+        properties.put("Maximum JVM memory usage", (RUNTIME.maxMemory() / ONE_MB) + " MB");
+        properties.put("Total JVM memory usage", (RUNTIME.totalMemory() / ONE_MB) + " MB");
 
-    private void setDetailedJavaPlatformCaps(JTextArea textArea)
-    {
-        textArea.append("\nuser.name:\t\t" + System.getProperty("user.name") + "\n");
-        String property = System.getProperty("java.library.path");
-        if ( property.startsWith("\\") )
-            property = property.substring( 1, property.length() );
-        textArea.append("java.library.path:\t" + property + "\n");
-        textArea.append("java.io.tmpdir:\t\t" + System.getProperty("java.io.tmpdir") + "\n");
-        textArea.append("\n");
-        textArea.append("sun.boot.library.path:\t" + System.getProperty("sun.boot.library.path") + "\n");
-        textArea.append("file.encoding.pkg:\t" + System.getProperty("file.encoding.pkg") + "\n");
-        textArea.append("user.country:\t\t" + System.getProperty("user.country") + "\n");
-        textArea.append("sun.java.launcher:\t" + System.getProperty("sun.java.launcher") + "\n");
-        textArea.append("user.dir:\t\t" + System.getProperty("user.dir") + "\n");
-        textArea.append("java.awt.graphicsenv:\t" + System.getProperty("java.awt.graphicsenv") + "\n");
-        textArea.append("java.endorsed.dirs:\t" + System.getProperty("java.endorsed.dirs") + "\n");
-        textArea.append("sun.java2d.opengl:\t" + System.getProperty("sun.java2d.opengl") + "\n");
-        textArea.append("sun.java2d.noddraw:\t" + System.getProperty("sun.java2d.noddraw") + "\n");
-        textArea.append("sun.jnu.encoding:\t" + System.getProperty("sun.jnu.encoding") + "\n");
-        textArea.append("java.class.version:\t" + System.getProperty("java.class.version") + "\n");
-        textArea.append("sun.management.compiler:\t" + System.getProperty("sun.management.compiler") + "\n");
-        textArea.append("user.home:\t\t" + System.getProperty("user.home") + "\n");
-        textArea.append("user.timezone:\t\t" + System.getProperty("user.timezone") + "\n");
-        textArea.append("java.awt.printerjob:\t" + System.getProperty("java.awt.printerjob") + "\n");
-        textArea.append("file.encoding:\t\t" + System.getProperty("file.encoding") + "\n");
-        textArea.append("java.specification.version:\t" + System.getProperty("java.specification.version") + "\n");
-        textArea.append("java.class.path:\t" + ( (IS_WIN) ? "\t" :"" ) + System.getProperty("java.class.path") + "\n");
-        textArea.append("java.vm.specification.version:\t" + System.getProperty("java.vm.specification.version") + "\n");
-        textArea.append("java.home:\t\t" + System.getProperty("java.home") + "\n");
-        textArea.append("user.language:\t\t" + System.getProperty("user.language") + "\n");
-        textArea.append("awt.toolkit:\t\t" + System.getProperty("awt.toolkit") + "\n");
-        textArea.append("java.vm.info:\t\t" + System.getProperty("java.vm.info") + "\n");
-        String[] propertyStringArray = System.getProperty("java.ext.dirs").split(";");
-        textArea.append("java.ext.dirs:\t\t" + propertyStringArray[0] + "\n");
-        for (int i = 1; i < propertyStringArray.length; i++)
-            textArea.append("\t\t" + propertyStringArray[i] + "\n");
-        propertyStringArray = System.getProperty("sun.boot.class.path").split(";");
-        textArea.append("sun.boot.class.path:\t" + propertyStringArray[0] + "\n");
-        for (int i = 1; i < propertyStringArray.length; i++)
-            textArea.append("\t\t" + propertyStringArray[i] + "\n");
-        textArea.append("sun.cpu.endian:\t" + System.getProperty("sun.cpu.endian") + "\n");
-        textArea.append("sun.io.unicode.encoding:\t" + System.getProperty("sun.io.unicode.encoding") + "\n");
-        textArea.append("sun.desktop:\t\t" + System.getProperty("sun.desktop") + "\n");
-        propertyStringArray = System.getProperty("sun.cpu.isalist").split("\\s|\\+");
-        textArea.append("sun.cpu.isalist:\t\t" + propertyStringArray[0] + "\n");
-        for (int i = 1; i < propertyStringArray.length; i++)
-            textArea.append("\t\t" + propertyStringArray[i] + "\n");
+        properties.putAll(System.getProperties());
+        appendPropertiesToTextArea(properties, textArea);
     }
 
     @Override
@@ -167,5 +155,5 @@ public final class LayoutJavaPlatformCapsDialog extends JDialog implements Actio
         return javaPlatformCapsAction;
     }
 
-    
+
 }
