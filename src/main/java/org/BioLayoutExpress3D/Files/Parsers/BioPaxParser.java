0e48379af532129f4dc9c2b28544c24ef42b0515
Derek Wright <Derek.Wright@glasgow.ac.uk>
2013-05-23 18:34:29 +0100

    Replaced parser with Paxtools

--- src/main/java/org/BioLayoutExpress3D/Files/Parsers/BioPaxParser.java
+++ src/main/java/org/BioLayoutExpress3D/Files/Parsers/BioPaxParser.java
@@ -2,15 +2,17 @@ package org.BioLayoutExpress3D.Files.Parsers;
 
 import java.io.*;
 import java.util.*;
-import java.util.regex.*;
+import java.util.logging.Logger;
 import org.BioLayoutExpress3D.CoreUI.*;
 import org.BioLayoutExpress3D.CoreUI.Dialogs.*;
 import org.BioLayoutExpress3D.Network.*;
 import static org.BioLayoutExpress3D.Environment.GlobalEnvironment.*;
-import static org.BioLayoutExpress3D.DebugConsole.ConsoleOutput.*;
+import org.BioLayoutExpress3D.Files.webservice.ImportWebService;
 import org.biopax.paxtools.io.BioPAXIOHandler;
 import org.biopax.paxtools.io.SimpleIOHandler;
 import org.biopax.paxtools.model.Model;
+import org.biopax.paxtools.model.level3.Entity;
+import org.biopax.paxtools.model.level3.Interaction;
 
 /**
 * Parser for BioPAX Level 3 OWL encoded as RDF/XML. 
@@ -22,21 +24,20 @@ import org.biopax.paxtools.model.Model;
 
 public final class BioPAXParser extends CoreParser
 {
-    private int counter = 0;
+    private static final Logger logger = Logger.getLogger(BioPAXParser.class.getName());
+    
     private BioPAXIOHandler handler;
 
     public BioPAXParser(NetworkContainer nc, LayoutFrame layoutFrame)
     {
         super(nc, layoutFrame);
-
-        counter = 0;
     }
 
     /**
      * Initialize the parser.
      * @param file - the file to be parsed
      * @param fileExtension - the extension of the file ("owl")
-     * @return true if parsing successful, false if unsuccessful
+     * @return true if parser initialized successfully, false if parser not initialized successfully
      */
     @Override
     public boolean init(File file, String fileExtension)
@@ -45,59 +46,28 @@ public final class BioPAXParser extends CoreParser
         
         //use JAXP reader rather than Jena reader as only XML files expected + handles large files with high performance        
         handler = new SimpleIOHandler(); //auto-detects BioPAX level, default level 3
-/*
-        try
-        {
-
-        
-            //fileReaderCounter  = new BufferedReader( new FileReader(file) );
-            //fileReaderBuffered = new BufferedReader( new FileReader(file) );
-
-            isSif = false;
-            simpleFileName = file.getName();
-
-            return true;
-        }
-        catch (Exception exc)
-        {
-            try
-            {
-                fileReaderCounter.close();
-                fileReaderBuffered.close();
-            }
-            catch (IOException ioe)
-            {
-                if (DEBUG_BUILD) println("IOException while closing streamers in CoreParser.init():\n" + ioe.getMessage());
-            }
-            finally
-            {
-
-            }
-
-            return false;
-        }
-      */
         return true;
     }
     
-    
+    /**
+     * Parse the OWL file and create a network.
+     * @return true if parsing successful, otherwise false
+     */
     @Override
     public boolean parse()
     {
-
+/*
         String currReaction = "";
         String currComplex = "";
         String currCat = "";
         String currParticipant = "";
         String controller = "";
         String controlled = "";
+        
         HashMap<String, ArrayList<String>> links = new HashMap<String, ArrayList<String>>();
         HashMap<String, ArrayList<String>> cats = new HashMap<String, ArrayList<String>>();
         HashMap<String, String> participants = new HashMap<String, String>();    
-        Matcher matcher = null;
-
-        int totalLines = 0;
-
+*/
         isSuccessful = false;
         nc.setOptimized(false);
 
@@ -105,255 +75,42 @@ public final class BioPAXParser extends CoreParser
 
         try
         {
-            Model model = handler.convertFromOWL(new FileInputStream(file)); //construct object model from OWL file
-
-            
-            /*
-            while ( ( line = fileReaderCounter.readLine() ) != null )
-                totalLines++;
-                */
-               
-            layoutProgressBarDialog.prepareProgressBar(1, "Parsing...");
+            int progressCounter = 0;
+            layoutProgressBarDialog.prepareProgressBar(3, "Parsing " + file.getName());
             layoutProgressBarDialog.startProgressBar();
+            layoutProgressBarDialog.incrementProgress(++progressCounter);
 
-            while ( ( line = fileReaderBuffered.readLine() ) != null )
-            {
-                layoutProgressBarDialog.incrementProgress(++counter);
-                length = line.length();
-                currentPos = 0;
-
-                if (length > 0)
+            Model model = handler.convertFromOWL(new FileInputStream(file)); //construct object model from OWL file
+            Set<Interaction> interactions = model.getObjects(Interaction.class);
+            
+            for (Interaction interaction : interactions) 
+            {    
+                logger.info(interaction.getDisplayName());
+                Set<Entity> participants = interaction.getParticipant();
+                for(Entity participant: participants)
                 {
-                    matcher = patternMatcher(line, ".*<bp:biochemicalReaction rdf:ID=\\\"([^\\\"]+)\\\".*");
-                    if ( matcher.matches() )
-                        currReaction = matcher.group(1);
-
-                    if ( patternMatcher(line, ".*<\\/bp:biochemicalReaction>.*").matches() )
-                        currReaction = "";
-
-                    if ( !currReaction.equals("") )
-                    {
-                        matcher = patternMatcher(line, ".*<bp:(\\S+) rdf:resource=\\\"#([^\\\"]+)\\\".*");
-                        if ( matcher.matches() )
-                        {
-                            if ( ( matcher.group(1).equals("LEFT") ) || ( matcher.group(1).equals("LEFT") ) )
-                            {
-                                String type = matcher.group(1);
-                                String target = matcher.group(2);
-                                ArrayList<String> a = null;
-                                if ( !links.containsKey(currReaction) )
-                                {
-                                    a = new ArrayList<String>();
-                                    links.put(currReaction, a);
-                                }
-                                else
-                                {
-                                    a = links.get(currReaction);
-                                }
-
-                                a.add(type + "\t" + target);
-                            }
-                        }
-                    }
-
-                    matcher = patternMatcher(line, ".*<bp:complex rdf:ID=\\\"([^\\\"]+)\\\".*");
-                    if ( matcher.matches() )
-                        currComplex = matcher.group(1);
-
-                    if ( patternMatcher(line, ".*<\\/bp:complex>.*").matches() )
-                        currComplex = "";
-
-                    if ( !currComplex.equals("") )
-                    {
-                        matcher = patternMatcher(line, ".*<bp:COMPONENTS rdf:resource=\\\"#([^\\\"]+)\\\".*");
-                        if ( matcher.matches() )
-                        {
-                            String type = "CONTAINS";
-                            String target = matcher.group(1);
-                            ArrayList<String> a = null;
-                            if ( !links.containsKey(currComplex) )
-                            {
-                                a = new ArrayList<String>();
-                                links.put(currComplex, a);
-                            }
-                            else
-                            {
-                                a = links.get(currComplex);
-                            }
-
-                            a.add(type + "\t" + target);
-                        }
-                    }
-
-                    matcher = patternMatcher(line, ".*<bp:catalysis rdf:ID=\\\"([^\\\"]+)\\\".*");
-                    if ( matcher.matches() )
-                        currCat = matcher.group(1);
-
-                    if ( patternMatcher(line, ".*<\\/bp:catalysis>.*").matches() )
-                    {
-                        currCat = "";
-                        controller = "";
-                        controlled = "";
-                    }
-
-                    if ( !currCat.equals("") )
-                    {
-                        String type = "";
-
-                        matcher = patternMatcher(line, ".*<bp:CONTROLLER rdf:resource=\\\"#([^\\\"]+)\\\".*");
-                        if ( matcher.matches() )
-                        {
-                            type = "CONTROLLER";
-                            controller = matcher.group(1);
-                        }
-
-                        matcher = patternMatcher(line, ".*<bp:CONTROLLED rdf:resource=\\\"#([^\\\"]+)\\\".*");
-                        if ( matcher.matches() )
-                        {
-                            type = "CONTROLLED";
-                            controlled = matcher.group(1);
-                        }
-
-                        matcher = patternMatcher(line, ".*<bp:CONTROL-TYPE rdf:datatype=\\\"[^\\\"]+\\\">([^<]+).*");
-                        if ( matcher.matches() )
-                        {
-                            type = matcher.group(1);
-                            ArrayList<String> a = null;
-                            if ( !cats.containsKey(controller) )
-                            {
-                                a = new ArrayList<String>();
-                                cats.put(controller, a);
-                            }
-                            else
-                            {
-                                a = cats.get(controller);
-                            }
-
-                            if (DEBUG_BUILD) println("Adding to cats:" + type + " " + controlled + " under " + controller);
-                            a.add(type + "\t" + controlled);
-                        }
-
-                    }
-
-                    matcher = patternMatcher(line, ".*<bp:\\S+Participant rdf:ID=\\\"([^\\\"]+)\\\".*");
-                    if ( matcher.matches() )
-                        currParticipant = matcher.group(1);
-
-                    if ( patternMatcher(line, ".*<\\/bp:physicalEntityParticipant>.*").matches() )
-                        currParticipant = "";
-
-                    if ( !currParticipant.equals("") )
-                    {
-                        matcher = patternMatcher(line, ".*<bp:PHYSICAL-ENTITY rdf:resource=\\\"#([^\\\"]+)\\\".*");
-                        if ( matcher.matches() )
-                            participants.put(currParticipant, matcher.group(1));
-                    }
-                }
+                    
+                    
+                    //nc.addNetworkConnection("test", participant.getDisplayName(), interaction.getDisplayName(), false, false, false);
+                    logger.info(participant.getDisplayName());
+                }                
             }
-
-            int count = 0;
-            Set<String> keys = links.keySet();
-            for (String key : keys)
-            {
-                if (DEBUG_BUILD) println(key);
-
-                ArrayList<String> a = links.get(key);
-                for (String entry : a)
-                {
-                    String type = entry.split("\t")[0];
-                    String target = entry.split("\t")[1];
-
-                    if (DEBUG_BUILD) println(key + " " + participants.get(target) + " " + type);
-
-                    nc.addNetworkConnection(key, type + counter, 0.0f);
-                    nc.addNetworkConnection(type + counter, participants.get(target), 0.0f);
-
-                    Vertex vertexMap = nc.getVerticesMap().get(type + counter);
-                    vertexMap.setVertexSize(vertexMap.getVertexSize() / 2);
-                    vertexMap.setPseudoVertex();
-
-                    LayoutClasses lc = null;
-                    VertexClass vc = null;
-
-                    if ( type.equals("CONTAINS") )
-                    {
-                        lc = nc.getLayoutClassSetsManager().getClassSet("COMPLEXES");
-                        vc = lc.createClass(key);
-                        lc.setClass(nc.getVerticesMap().get(participants.get(target)), vc);
-                    }
-
-                    lc = nc.getLayoutClassSetsManager().getClassSet(0);
-                    vc = lc.createClass(type);
-                    lc.setClass(nc.getVerticesMap().get(type + ++count), vc);
-                }
-            }
-
-            keys = cats.keySet();
-            for (String key : keys)
-            {
-                if (DEBUG_BUILD) println(key);
-
-                ArrayList<String> a = cats.get(key);
-                for (String entry : a)
-                {
-                    counter++;
-                    String type = entry.split("\t")[0];
-                    String target = entry.split("\t")[1];
-
-                    if (DEBUG_BUILD) println(participants.get(key) + "\t" + target + "\t1.0\t" + type);
-
-                    nc.addNetworkConnection(participants.get(key), target + counter, 0.0f);
-                    nc.addNetworkConnection(target + counter, type, 0.0f);
-
-                    Vertex vertexMap = nc.getVerticesMap().get(target + counter);
-                    vertexMap.setVertexSize(vertexMap.getVertexSize() / 2);
-                    vertexMap.setPseudoVertex();
-
-                    LayoutClasses lc = null;
-                    VertexClass vc = null;
-
-                    lc = nc.getLayoutClassSetsManager().getClassSet(0);
-                    vc = lc.createClass(target);
-                    lc.setClass(nc.getVerticesMap().get(target + counter), vc);
-                }
-            }
-
+               
+            layoutProgressBarDialog.incrementProgress(++progressCounter);
+           
             isSuccessful = true;
         }
-        catch (IOException ioe)
+        catch(FileNotFoundException e)
         {
-            if (DEBUG_BUILD) println("IOException in BioPaxParser.parse():\n" + ioe.getMessage());
+            //TODO display error dialogue
+            logger.warning(e.getMessage());
+            return false;
         }
         finally
         {
-            /*
-            try
-            {
-                //fileReaderCounter.close();
-                //fileReaderBuffered.close();
-            }
-            catch (IOException ioe)
-            {
-                if (DEBUG_BUILD) println("IOException while closing streams in BioPaxParser.parse():\n" + ioe.getMessage());
-            }
-            finally
-            {
-                layoutProgressBarDialog.endProgressBar();
-            }
-            */
             layoutProgressBarDialog.endProgressBar();
         }
 
         return isSuccessful;
     }
-
-    private Matcher patternMatcher(String string, String patrn)
-    {
-        Pattern pattern = Pattern.compile(patrn);
-        Matcher matcher = pattern.matcher(string);
-
-        return matcher;
-    }
-
-
 }
