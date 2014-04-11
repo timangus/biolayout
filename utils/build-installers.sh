14e2338728bfacb2baf89c8dc2d8e5e0126942bd
Tim Angus <tim@ngus.net>
2014-04-11 13:41:11 +0100

    Set write permissions on .dmg contents

--- utils/build-installers.sh
+++ utils/build-installers.sh
@@ -35,7 +35,8 @@ cp ${SRC_DIR}/nsis-installer/${BASE_NAME}-${VERSION}-installer.exe ${BUILD_DIR}
 
 # OS X
 cd ${SRC_DIR}/target
-genisoimage -D -V BioLayoutExpress3D -no-pad -r -apple -o ${BUILD_DIR}/${BASE_NAME}-${VERSION}.dmg dmg/
+genisoimage -D -V BioLayoutExpress3D -no-pad -uid 0 -gid 0 -dir-mode 755 -file-mode 644 \
+    -apple -o ${BUILD_DIR}/${BASE_NAME}-${VERSION}.dmg dmg/
 
 # Everything else
 cp ${SRC_DIR}/target/${BASE_NAME}-${VERSION}.jar ${BUILD_DIR}
