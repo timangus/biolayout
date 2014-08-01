b34bbb4c240843919d938a52d19cd442017ef864
Tim Angus <tim@ngus.net>
2014-08-01 16:24:43 +0100

    Put the colours in a different order

--- src/main/java/org/BioLayoutExpress3D/Network/VertexClassColorAssigner.java
+++ src/main/java/org/BioLayoutExpress3D/Network/VertexClassColorAssigner.java
@@ -16,21 +16,21 @@ public class VertexClassColorAssigner
     static
     {
         DISTINCT_COLORS.add(new Color(0,   0,   0  )); // Black
-        DISTINCT_COLORS.add(new Color(87,  87,  87 )); // Dark grey
-        DISTINCT_COLORS.add(new Color(160, 160, 160)); // Light grey
+        DISTINCT_COLORS.add(new Color(29,  105, 20 )); // Green
+        DISTINCT_COLORS.add(new Color(233, 222, 187)); // Tan
+        DISTINCT_COLORS.add(new Color(255, 205, 243)); // Pink
+        DISTINCT_COLORS.add(new Color(41,  208, 208)); // Cyan
+        DISTINCT_COLORS.add(new Color(129, 197, 122)); // Light green
+        DISTINCT_COLORS.add(new Color(129, 74,  25 )); // Brown
         DISTINCT_COLORS.add(new Color(255, 255, 255)); // White
         DISTINCT_COLORS.add(new Color(42,  75,  215)); // Blue
-        DISTINCT_COLORS.add(new Color(29,  105, 20 )); // Green
-        DISTINCT_COLORS.add(new Color(129, 74,  25 )); // Brown
-        DISTINCT_COLORS.add(new Color(129, 38,  192)); // Purple
-        DISTINCT_COLORS.add(new Color(157, 175, 255)); // Light blue
-        DISTINCT_COLORS.add(new Color(129, 197, 122)); // Light green
-        DISTINCT_COLORS.add(new Color(233, 222, 187)); // Tan
         DISTINCT_COLORS.add(new Color(173, 35,  35 )); // Red
-        DISTINCT_COLORS.add(new Color(41,  208, 208)); // Cyan
         DISTINCT_COLORS.add(new Color(255, 238, 51 )); // Yellow
+        DISTINCT_COLORS.add(new Color(160, 160, 160)); // Light grey
+        DISTINCT_COLORS.add(new Color(157, 175, 255)); // Light blue
+        DISTINCT_COLORS.add(new Color(129, 38,  192)); // Purple
         DISTINCT_COLORS.add(new Color(255, 146, 51 )); // Orange
-        DISTINCT_COLORS.add(new Color(255, 205, 243)); // Pink
+        DISTINCT_COLORS.add(new Color(87,  87,  87 )); // Dark grey
     }
 
     private int nextColorIndex;
@@ -57,9 +57,9 @@ public class VertexClassColorAssigner
         }
 
         Color color = DISTINCT_COLORS.get(nextColorIndex);
-        int r = (color.getRed() + (numShifts * SHIFT_VALUE)) % 255;
-        int g = (color.getGreen() + (numShifts * SHIFT_VALUE)) % 255;
-        int b = (color.getBlue() + (numShifts * SHIFT_VALUE)) % 255;
+        int r = (color.getRed() + (numShifts * SHIFT_VALUE)) % 256;
+        int g = (color.getGreen() + (numShifts * SHIFT_VALUE)) % 256;
+        int b = (color.getBlue() + (numShifts * SHIFT_VALUE)) % 256;
 
         color = new Color(r, g, b);
         assignedColors.put(name, color);
