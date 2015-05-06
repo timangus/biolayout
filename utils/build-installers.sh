1d80b49d5267a6f4a795346bee00c5a797b9f0cc
Tim Angus <tim@ngus.net>
2015-05-06 11:58:54 +0100

    Update Mac build

--- utils/build-installers.sh
+++ utils/build-installers.sh
@@ -69,7 +69,7 @@ cp ${SRC_DIR}/nsis-installer/${OUTPUT_NAME}-${VERSION}-installer.exe ${BUILD_DIR
 
 # OS X
 cd ${SRC_DIR}/target
-genisoimage -D -V Kajeka -no-pad -uid 0 -gid 0 -dir-mode 755 -file-mode 644 \
+genisoimage -D -V ${OUTPUT_NAME} -no-pad -uid 0 -gid 0 -dir-mode 755 -file-mode 644 \
     -apple -o ${BUILD_DIR}/${OUTPUT_NAME}-${VERSION}.dmg dmg/
 
 # Everything else
