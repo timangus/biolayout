e15c3c12b9a0b54f315120c497e9b4cb879ba46f
Tim Angus <tim@ngus.net>
2017-11-01 15:25:11 +0000

    New icon

---
+++ src/main/resources/Resources/Images/generate.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=$(dirname ${SCRIPT})
+ICON_FILE=$1
+
+if [ ! -e ${ICON_FILE} ]
+then
+  echo ${ICON_FILE} does not exist!
+  exit 1
+fi
+
+BASE_NAME="${ICON_FILE%.*}"
+
+for SIZE in 512 256 128 64 48 32 16
+do
+  rsvg -w ${SIZE} -h ${SIZE} ${BASE_NAME}.svg ${BASE_NAME}${SIZE}x${SIZE}.png
+done
+
+function iconFilesOfSize()
+{
+  PNGS=
+  for PNG in $(ls ${SCRIPT_DIR}/${BASE_NAME}*.png);
+  do
+    VALID_SIZES="$1"
+    SIZE=$(convert ${PNG} -print "%w\n" /dev/null)
+
+    if [[ ${VALID_SIZES} =~ ${SIZE} ]];
+    then
+      PNGS="${PNGS} ${PNG}"
+    fi
+  done
+
+  echo ${PNGS}
+}
+
+icotool -c $(iconFilesOfSize "256 128 64 48 32 16") -o ${SCRIPT_DIR}/${BASE_NAME}.ico
